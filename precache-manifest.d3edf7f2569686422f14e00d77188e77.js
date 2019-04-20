7e693815019b63f59981be6e0f357e4b6573b1c0
cjhyy <37287022+cjhyy@users.noreply.github.com>
2019-04-20 14:53:38 +0800

    Updates

---
+++ precache-manifest.d3edf7f2569686422f14e00d77188e77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2204c0f13741eca4f255",
+    "url": "/hackernew/static/js/runtime~main.e50e1d16.js"
+  },
+  {
+    "revision": "ddaa2b1d00a0179e8484",
+    "url": "/hackernew/static/js/main.4c516f88.chunk.js"
+  },
+  {
+    "revision": "d54eb5b70f002944987e",
+    "url": "/hackernew/static/js/2.9a4e8d7f.chunk.js"
+  },
+  {
+    "revision": "ddaa2b1d00a0179e8484",
+    "url": "/hackernew/static/css/main.d71a4d5d.chunk.css"
+  },
+  {
+    "revision": "d54eb5b70f002944987e",
+    "url": "/hackernew/static/css/2.9345ce26.chunk.css"
+  },
+  {
+    "revision": "61e137b25517ef5066585b9bb7eb40ed",
+    "url": "/hackernew/index.html"
+  }
+];
